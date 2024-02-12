9eb53cd5058aba4e258ee9a6465864eebf7214a0
Prune <prune@lecentre.net>
2024-02-12 11:46:09 -0500

    added pages github workflow

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -3,6 +3,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://kubecolor.github.io',
+  	base: '/kubecolor',
 	integrations: [
 		starlight({
 			title: 'KubeColor',
