c41dc6f4ab54f8b6c7bbb4b7436aa4dac901ffa3
Kalle Fagerberg <kalle.fagerberg@riskident.com>
2024-08-16 17:38:21 +0200

    Colorize kubectl drain and uncordon

--- printer/kubectl_verb.go
+++ printer/kubectl_verb.go
@@ -16,16 +16,29 @@ import (
 //	deployment.apps/bar created
 //	deployment.apps/quux configured
 //
-// But it is also used to colorize:
+// But it is also used to colorize commands like:
 //
 // - kubectl create
 // - kubectl delete
+// - kubectl drain
 // - kubectl expose
 // - kubectl patch
+// - kubectl uncordon
 type VerbPrinter struct {
-	DryRunColor   config.Color
+	// VerbColor is used for verbs at the end of the line (followed by optional "dry-run"), e.g:
+	// 	 pod/nginx-28729634-nh2vc evicted
+	// 	 pod "nginx-28729634-nh2vc" deleted (dry run)
+	VerbColor map[string]config.Color
+
+	// PrefixVerbColor is used for verbs that are prefixes instead of suffixes, e.g:
+	// 	 evicting pod nginx/nginx-28729634-nh2vc
+	PrefixVerbColor map[string]config.Color
+
+	// DryRunColor is used on the "(dry run)" or "(server dry run)" suffix of a line.
+	DryRunColor config.Color
+
+	// FallbackColor is used when no verbs has matched on the output line.
 	FallbackColor config.Color
-	VerbColor     map[string]config.Color
 }
 
 // ensures it implements the interface
@@ -59,19 +72,36 @@ func (p *VerbPrinter) Print(r io.Reader, w io.Writer) {
 
 func (p *VerbPrinter) colorizeVerb(line string) (string, bool) {
 	for verb, color := range p.VerbColor {
-		idx := strings.Index(line, verb)
+		idx := strings.LastIndex(line, verb)
 		if idx == -1 || idx == 0 {
 			// don't colorize first word
 			continue
 		}
+		before := line[:idx]
+		after := line[idx+len(verb):]
 
-		if line[idx-1] != ' ' {
+		if !strings.HasSuffix(before, " ") {
 			// must be after a space
 			continue
 		}
 
-		return fmt.Sprintf("%s%s%s", line[:idx], color.Render(verb), line[idx+len(verb):]), true
+		return fmt.Sprintf("%s%s%s", before, color.Render(verb), after), true
 	}
+
+	for verbPrefix, color := range p.PrefixVerbColor {
+		after, ok := strings.CutPrefix(line, verbPrefix)
+		if !ok {
+			continue
+		}
+
+		if !strings.HasPrefix(after, " ") {
+			// must be before a space
+			continue
+		}
+
+		return color.Render(verbPrefix) + after, true
+	}
+
 	return line, false
 }
 
