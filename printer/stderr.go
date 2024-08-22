789107ee3e218b875a707bf384a9df4739771d45
kalle (jag) <2477952+applejag@users.noreply.github.com>
2024-08-22 04:12:50 +0200

    Colorize stderr logs using "kubectl logs" logic (#166)
    * Colorize stderr logs using "kubectl logs" logic
    
    * Fix comment

--- printer/stderr.go
+++ printer/stderr.go
@@ -20,30 +20,42 @@ var _ Printer = &StderrPrinter{}
 // Print implements [Printer.Print]
 func (p *StderrPrinter) Print(r io.Reader, w io.Writer) {
 	scanner := bufio.NewScanner(r)
+
+	logsPrinter := LogsPrinter{
+		Theme: p.Theme,
+	}
+	logsPrinterReader := strings.NewReader("")
+
 	for scanner.Scan() {
 		line := scanner.Text()
-		fmt.Fprintln(w, p.formatLine(line))
+		if s, ok := p.formatLine(line); ok {
+			fmt.Fprintln(w, s)
+			continue
+		}
+
+		logsPrinterReader.Reset(line)
+		logsPrinter.Print(logsPrinterReader, w)
 	}
 }
 
-func (p *StderrPrinter) formatLine(line string) string {
+func (p *StderrPrinter) formatLine(line string) (string, bool) {
 	if strings.HasPrefix(strings.ToLower(line), "error") {
-		return p.Theme.Stderr.Error.Render(line)
+		return p.Theme.Stderr.Error.Render(line), true
 	}
 
 	if after, ok := strings.CutPrefix(line, "No resources found"); ok {
 		if after == "" {
-			return p.Theme.Stderr.NoneFound.Render(line)
+			return p.Theme.Stderr.NoneFound.Render(line), true
 		}
 		if afterIn, ok := strings.CutPrefix(after, " in "); ok {
 			if ns, ok := strings.CutSuffix(afterIn, " namespace."); ok {
 				return p.Theme.Stderr.NoneFound.Render(fmt.Sprintf(
 					"No resources found in %s namespace.",
 					p.Theme.Stderr.NoneFoundNamespace.Render(ns),
-				))
+				)), true
 			}
 		}
 	}
 
-	return p.Theme.Stderr.Default.Render(line)
+	return "", false
 }
