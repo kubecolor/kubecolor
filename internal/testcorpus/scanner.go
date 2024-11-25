596f6b2bab8ae1775efb6e9632b5cdbe3625a9fc
Marcel Herhold <herhold.marcel@gmail.com>
2024-11-25 07:40:41 +0100

    Format

--- internal/testcorpus/scanner.go
+++ internal/testcorpus/scanner.go
@@ -1,168 +1,168 @@
 package testcorpus
 
 import (
-  "bufio"
-  "errors"
-  "fmt"
-  "io"
-  "regexp"
-  "slices"
-  "strings"
-  "unicode/utf8"
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"regexp"
+	"slices"
+	"strings"
+	"unicode/utf8"
 )
 
 func NewTestScanner(reader io.Reader) *TestScanner {
-  return &TestScanner{
-    scanner: bufio.NewScanner(reader),
-    first:   true,
-  }
+	return &TestScanner{
+		scanner: bufio.NewScanner(reader),
+		first:   true,
+	}
 }
 
 type TestScanner struct {
-  scanner *bufio.Scanner
+	scanner *bufio.Scanner
 
-  test  Test
-  err   error
-  done  bool
-  first bool
+	test  Test
+	err   error
+	done  bool
+	first bool
 }
 
 func (s *TestScanner) Scan() bool {
-  s.err = s.scanErr()
-  return s.err == nil && !s.done
+	s.err = s.scanErr()
+	return s.err == nil && !s.done
 }
 
 func (s *TestScanner) Test() Test {
-  return s.test
+	return s.test
 }
 
 func (s *TestScanner) Err() error {
-  return s.err
+	return s.err
 }
 
 func (s *TestScanner) scanErr() error {
-  if s.first {
-    s.first = false
-    if _, err := scanLinesUntilCharLine(s.scanner, "="); errors.Is(err, io.ErrUnexpectedEOF) {
-      // no tests found
-      s.done = true
-      return nil
-    } else if err != nil {
-      return err
-    }
-  }
+	if s.first {
+		s.first = false
+		if _, err := scanLinesUntilCharLine(s.scanner, "="); errors.Is(err, io.ErrUnexpectedEOF) {
+			// no tests found
+			s.done = true
+			return nil
+		} else if err != nil {
+			return err
+		}
+	}
 
-  s.test = Test{}
-  if err := s.scanHeader(); err != nil {
-    if errors.Is(err, io.ErrUnexpectedEOF) {
-      s.done = true
-      return nil
-    }
-    return err
-  }
-  if err := s.scanInput(); err != nil {
-    return err
-  }
-  if err := s.scanOutput(); err != nil {
-    return err
-  }
-  return nil
+	s.test = Test{}
+	if err := s.scanHeader(); err != nil {
+		if errors.Is(err, io.ErrUnexpectedEOF) {
+			s.done = true
+			return nil
+		}
+		return err
+	}
+	if err := s.scanInput(); err != nil {
+		return err
+	}
+	if err := s.scanOutput(); err != nil {
+		return err
+	}
+	return nil
 }
 
 func (s *TestScanner) scanHeader() error {
-  headerLines, err := scanLinesUntilCharLine(s.scanner, "=")
-  if err != nil {
-    return fmt.Errorf("scan until closing === line: %w", err)
-  }
-  for _, line := range headerLines {
-    if err := s.readHeaderLine(line); err != nil {
-      return err
-    }
-  }
-  return nil
+	headerLines, err := scanLinesUntilCharLine(s.scanner, "=")
+	if err != nil {
+		return fmt.Errorf("scan until closing === line: %w", err)
+	}
+	for _, line := range headerLines {
+		if err := s.readHeaderLine(line); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func (s *TestScanner) readHeaderLine(line string) error {
-  trimmed := strings.TrimSpace(line)
-  if trimmed == "" {
-    return nil
-  }
-  firstChar, size := utf8.DecodeRuneInString(line)
-  rest := strings.TrimSpace(trimmed[size:])
-  switch firstChar {
-  case '#':
-    s.test.Name = rest
-    return nil
-  case '$':
-    if s.test.Command != "" {
-      return fmt.Errorf("test %q: found multiple command lines (starting with '$') in header", s.test.Name)
-    }
-    s.test.Command = rest
-    if s.test.Name == "" {
-      s.test.Name = rest
-    }
-    return nil
-  }
+	trimmed := strings.TrimSpace(line)
+	if trimmed == "" {
+		return nil
+	}
+	firstChar, size := utf8.DecodeRuneInString(line)
+	rest := strings.TrimSpace(trimmed[size:])
+	switch firstChar {
+	case '#':
+		s.test.Name = rest
+		return nil
+	case '$':
+		if s.test.Command != "" {
+			return fmt.Errorf("test %q: found multiple command lines (starting with '$') in header", s.test.Name)
+		}
+		s.test.Command = rest
+		if s.test.Name == "" {
+			s.test.Name = rest
+		}
+		return nil
+	}
 
-  if key, value, ok := strings.Cut(trimmed, "="); ok {
-    key = strings.TrimSpace(key)
-    value = strings.TrimSpace(value)
-    if strings.HasPrefix(value, `"`) && strings.HasSuffix(value, `"`) {
-      if _, err := fmt.Sscanf(value, "%q", &value); err != nil {
-        return fmt.Errorf("test %q: env %q: failed to parse value as quoted string: %w", s.test.Name, key, err)
-      }
-    }
-    if slices.ContainsFunc(s.test.Env, func(e EnvVar) bool {
-      return e.Key == key
-    }) {
-      return fmt.Errorf("test %q: env %q: key specified multiple times", s.test.Name, key)
-    }
+	if key, value, ok := strings.Cut(trimmed, "="); ok {
+		key = strings.TrimSpace(key)
+		value = strings.TrimSpace(value)
+		if strings.HasPrefix(value, `"`) && strings.HasSuffix(value, `"`) {
+			if _, err := fmt.Sscanf(value, "%q", &value); err != nil {
+				return fmt.Errorf("test %q: env %q: failed to parse value as quoted string: %w", s.test.Name, key, err)
+			}
+		}
+		if slices.ContainsFunc(s.test.Env, func(e EnvVar) bool {
+			return e.Key == key
+		}) {
+			return fmt.Errorf("test %q: env %q: key specified multiple times", s.test.Name, key)
+		}
 
-    s.test.Env = append(s.test.Env, EnvVar{
-      Key:   key,
-      Value: value,
-    })
-    return nil
-  }
+		s.test.Env = append(s.test.Env, EnvVar{
+			Key:   key,
+			Value: value,
+		})
+		return nil
+	}
 
-  return fmt.Errorf("test %q: invalid test header line %q", s.test.Name, trimmed)
+	return fmt.Errorf("test %q: invalid test header line %q", s.test.Name, trimmed)
 }
 
 func (s *TestScanner) scanInput() error {
-  inputLines, err := scanLinesUntilCharLine(s.scanner, "-")
-  if err != nil {
-    return fmt.Errorf("scan test input: %w", err)
-  }
-  s.test.Input = strings.TrimSpace(strings.Join(inputLines, "\n"))
-  return nil
+	inputLines, err := scanLinesUntilCharLine(s.scanner, "-")
+	if err != nil {
+		return fmt.Errorf("scan test input: %w", err)
+	}
+	s.test.Input = strings.TrimSpace(strings.Join(inputLines, "\n"))
+	return nil
 }
 
 func (s *TestScanner) scanOutput() error {
-  outputLines, err := scanLinesUntilCharLine(s.scanner, "=")
-  if err != nil && !errors.Is(err, io.ErrUnexpectedEOF) {
-    return fmt.Errorf("scan test output: %w", err)
-  }
-  s.test.Output = strings.TrimSpace(strings.Join(outputLines, "\n"))
-  return nil
+	outputLines, err := scanLinesUntilCharLine(s.scanner, "=")
+	if err != nil && !errors.Is(err, io.ErrUnexpectedEOF) {
+		return fmt.Errorf("scan test output: %w", err)
+	}
+	s.test.Output = strings.TrimSpace(strings.Join(outputLines, "\n"))
+	return nil
 }
 
 func scanLinesUntilCharLine(scanner *bufio.Scanner, char string) ([]string, error) {
-  var lines []string
-  minNumberOfChars := 3
+	var lines []string
+	minNumberOfChars := 3
 
-  for scanner.Scan() {
-    line := strings.Trim(scanner.Text(), "\r")
+	for scanner.Scan() {
+		line := strings.Trim(scanner.Text(), "\r")
 
-    lineFilter := regexp.MustCompile(fmt.Sprintf(`^%s{%d,}$`, regexp.QuoteMeta(char), minNumberOfChars))
-    if lineFilter.MatchString(line) {
-      return lines, nil
-    }
+		lineFilter := regexp.MustCompile(fmt.Sprintf(`^%s{%d,}$`, regexp.QuoteMeta(char), minNumberOfChars))
+		if lineFilter.MatchString(line) {
+			return lines, nil
+		}
 
-    lines = append(lines, line)
-  }
-  if err := scanner.Err(); err != nil {
-    return nil, err
-  }
+		lines = append(lines, line)
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
 
-  return lines, io.ErrUnexpectedEOF
+	return lines, io.ErrUnexpectedEOF
 }
--- printer/kubectl_diff.go
+++ printer/kubectl_diff.go
@@ -1,45 +1,45 @@
 package printer
 
 import (
-  "bufio"
-  "fmt"
-  "github.com/kubecolor/kubecolor/config"
-  "io"
-  "regexp"
+	"bufio"
+	"fmt"
+	"github.com/kubecolor/kubecolor/config"
+	"io"
+	"regexp"
 )
 
 type DiffPrinter struct {
-  Theme *config.Theme
+	Theme *config.Theme
 }
 
 func (p *DiffPrinter) Print(r io.Reader, w io.Writer) {
-  scanner := bufio.NewScanner(r)
-  for scanner.Scan() {
-    line := scanner.Text()
-    if line == "" {
-      fmt.Fprintln(w, line)
-      continue
-    }
-    parsedLine := p.parseLine(line)
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			fmt.Fprintln(w, line)
+			continue
+		}
+		parsedLine := p.parseLine(line)
 
-    fmt.Fprintln(w, parsedLine)
-  }
+		fmt.Fprintln(w, parsedLine)
+	}
 }
 
 func (p *DiffPrinter) parseLine(line string) string {
-  theme := p.Theme.Diff
-  switch {
-  // All only lines, that starts with a single "+"and anything afterward
-  // e.g. + foo
-  // not allowed: ++ foo
-  case regexp.MustCompile(`^\+[^+]+.*$`).MatchString(line):
-    return theme.Added.Render(line)
-  // All only lines, that starts with a single "-" and anything afterward
-  // e.g. - foo
-  // not allowed: -- foo
-  case regexp.MustCompile(`^-[^-]+.*$`).MatchString(line):
-    return theme.Removed.Render(line)
-  default:
-    return theme.Unchanged.Render(line)
-  }
+	theme := p.Theme.Diff
+	switch {
+	// All only lines, that starts with a single "+"and anything afterward
+	// e.g. + foo
+	// not allowed: ++ foo
+	case regexp.MustCompile(`^\+[^+]+.*$`).MatchString(line):
+		return theme.Added.Render(line)
+	// All only lines, that starts with a single "-" and anything afterward
+	// e.g. - foo
+	// not allowed: -- foo
+	case regexp.MustCompile(`^-[^-]+.*$`).MatchString(line):
+		return theme.Removed.Render(line)
+	default:
+		return theme.Unchanged.Render(line)
+	}
 }
